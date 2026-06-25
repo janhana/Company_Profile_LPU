5a4a316d3996fe7aa39a863cc63a65fd27d8430b
hana <j4nhana@gmail.com>
2026-06-26 00:26:53 +0700

    fix koneksi

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = $_ENV['MYSQLHOST'];
+$user = $_ENV['MYSQLUSER'];
+$password = $_ENV['MYSQLPASSWORD'];
+$database = $_ENV['MYSQLDATABASE'];
+$port = $_ENV['MYSQLPORT'];
+
+$koneksi = mysqli_connect($host, $user, $password, $database, $port);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
